e16cc8412d52dcda1e26dd2ccea724162dd00909
sabbir hossain <mdsabbir477470@gmail.com>
2022-10-25 21:44:02 +0600

    cached node_modules

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
     res.send('Course API Running');
@@ -14,6 +15,24 @@ app.get('/', (req, res) => {
 app.get('/courses-categories', (req, res) => {
     res.send(categories);
 });
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    const category_courses = courses.filter(c => c.category_id === id);
+    console.log(category_courses);
+    res.send(category_courses)
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+});
+
+app.get('/course-details/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find(c => c._id === id);
+    res.send(selectedCourse)
+})
+
 
 app.listen(port, () => {
     console.log('course server running on port', port);
