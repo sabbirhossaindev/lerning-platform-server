501a510eb26406d660f92e1c57ce2c436f10f4cc
sabbir hossain <mdsabbir477470@gmail.com>
2022-10-25 10:23:00 +0600

    server start

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('News API Running');
+});
+// category id 
+
+
+app.listen(port, () => {
+    console.log('Dragon news server running on port', port);
+});
